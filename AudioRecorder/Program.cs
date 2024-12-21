4d129d548193760d46b1de79eb630c0da7207515
mrchysen <mrchysen@mail.ru>
2024-12-21 21:43:58 +0700

    Добавьте файлы проекта.

---
+++ AudioRecorder/Program.cs
@@ -0,0 +1,28 @@
+﻿using NAudio.Wave;
+
+var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+Directory.CreateDirectory(outputFolder);
+var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+
+using var waveIn = new WaveInEvent();
+using var writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+
+waveIn.DataAvailable += (s, a) =>
+{
+    writer.Write(a.Buffer, 0, a.BytesRecorded);
+    if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 30)
+    {
+        waveIn.StopRecording();
+    }
+};
+
+Console.WriteLine("Record is started");
+
+waveIn.StartRecording();
+
+await Task.Delay(5000);
+
+waveIn.StopRecording();
+
+Console.WriteLine("Record is over");
+Console.WriteLine("Path:" + outputFilePath);
